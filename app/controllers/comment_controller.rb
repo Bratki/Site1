315851d996b790b9047a314bd11645063c46763b
EugeneBel <yauheni.trayanovich@gmail.com>
2012-11-27 11:07:15 -0800

    Подправил show

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -2,9 +2,10 @@ class CommentController < ApplicationController
   def new
   	@fireplace = Fireplace.find(params[:id])
 	@comment = @fireplace.comments.build(params[:comment])
+	if (!((params[:comment][:name]=="")||(params[:comment][:content]=="")))
 	@comment.save
-	
+	end
 
-	redirect_to "fireplace/#{params[:id]}"
+	redirect_to "/fireplace/#{params[:id]}"
   end
 end
